7eea28f94d5ce4ed6aef73767447a3e3f0b6ab1d
Kugayama Nana <nonamea774@gmail.com>
2019-04-23 17:47:50 +0900

    initial commit

---
+++ lib/rack/auth/nginx_omniauth_adapter.rb
@@ -0,0 +1,10 @@
+require "rack/auth/nginx_omniauth_adapter/version"
+
+module Rack
+  module Auth
+    module NginxOmniauthAdapter
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rack/auth/nginx_omniauth_adapter/version.rb
@@ -0,0 +1,7 @@
+module Rack
+  module Auth
+    module NginxOmniauthAdapter
+      VERSION = "0.1.0"
+    end
+  end
+end
